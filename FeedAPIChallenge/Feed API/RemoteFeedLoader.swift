0ff0549da3476971aa99110034ce4475a398eadd
Mauro Worobiej <mauro.worobiej@kavak.com>
2021-09-05 14:07:24 -0300

    Decode data into a private 'ItemModel' and map those items to 'FeedImage' model

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -20,14 +20,37 @@ public final class RemoteFeedLoader: FeedLoader {
 
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
 		client.get(from: url) { result in
+
 			switch result {
-			case let .success((_, resonse)):
-				if resonse.statusCode != 200 {
-					completion(.failure(Error.invalidData))
+			case let .success((data, resonse)):
+				guard resonse.statusCode != 200,
+				      let items = try? JSONDecoder().decode([ItemModel].self, from: data)
+				else {
+					return completion(.failure(Error.invalidData))
 				}
+				completion(.success(items.map{ $0.feedImage }))
+				
 			case .failure:
 				completion(.failure(Error.connectivity))
 			}
 		}
+	}
+}
+
+private struct ItemModel: Decodable {
+	let id: UUID
+	let description: String?
+	let location: String?
+	let url: URL
+
+	enum CodingKeys: String, CodingKey {
+		case id = "image_id"
+		case description = "image_desc"
+		case location = "image_loc"
+		case url = "image_url"
+	}
+
+	var feedImage: FeedImage {
+		FeedImage(id: id, description: description, location: location, url: url)
 	}
 }
